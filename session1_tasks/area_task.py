d2124f1e42240a5994c0338ec53e2fb0881b3041
Mohamed khedr <80976641+Mohamedkhedr999@users.noreply.github.com>
2023-08-09 18:56:31 +0300

    Delete session1_tasks directory

--- session1_tasks/area_task.py
+++
@@ -1,7 +0,0 @@
- ##################################area of circle##################################
- 
-import math
-
-r = int(input("please enter the radius: "))
-area = math.pi * r**2
-print(area)
--- session1_tasks/check_vowel.py
+++
@@ -1,8 +0,0 @@
-##############################check vowel#############################################
-l = input("enter letter: ")
-vowels = ('a', 'e', 'i', 'o', 'u')
-
-if l.lower() in vowels:
-    print("this letter "+l+" is vowel")
-else:
-    print("this letter "+l+" is not vowel")
